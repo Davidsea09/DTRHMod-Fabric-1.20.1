26295c6a5d504bd82386338b79f05cfb38279313
Emanuel <ejdf09@gmail.com>
2025-08-10 15:38:44 -0400

    Initial commit

---
+++ src/main/java/net/emanueljdf09/dtrhmod/DownTheRabbitHoleClient.java
@@ -0,0 +1,10 @@
+package net.emanueljdf09.dtrhmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DownTheRabbitHoleClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/emanueljdf09/dtrhmod/DownTheRabbitHoleDataGenerator.java
@@ -0,0 +1,11 @@
+package net.emanueljdf09.dtrhmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DownTheRabbitHoleDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
